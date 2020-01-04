db72e1a4a17f0dd8b94c5554666b8dda45067992
JHyeok <dev.jhyeok@gmail.com>
2020-01-04 18:10:12 +0900

    서울에서김서방찾기 추가

---
+++ src/main/level1/서울에서김서방찾기/OtherSolution.java
@@ -0,0 +1,19 @@
+package main.level1.서울에서김서방찾기;
+
+import java.util.Arrays;
+
+public class OtherSolution {
+    public String findKim(String[] seoul) {
+        //x에 김서방의 위치를 저장하세요.
+        int x = Arrays.asList(seoul).indexOf("Kim");
+
+        return "김서방은 " + x + "에 있다";
+    }
+
+    // 실행을 위한 테스트코드입니다.
+    public static void main(String[] args) {
+        OtherSolution kim = new OtherSolution();
+        String[] names = {"Queen", "Tod", "Kim"};
+        System.out.println(kim.findKim(names));
+    }
+}
---
+++ src/main/level1/서울에서김서방찾기/Solution.java
@@ -0,0 +1,16 @@
+package main.level1.서울에서김서방찾기;
+
+public class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+
+        for (int i = 0; i < seoul.length; i++) {
+            if ("Kim".equals(seoul[i])) {
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
