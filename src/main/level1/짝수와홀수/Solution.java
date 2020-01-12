a887fd181595db18d9467fb285fea7496ba27fb4
JHyeok <dev.jhyeok@gmail.com>
2020-01-12 19:35:21 +0900

    짝수와홀수 추가

---
+++ src/main/level1/짝수와홀수/Solution.java
@@ -0,0 +1,15 @@
+package main.level1.짝수와홀수;
+
+public class Solution {
+    public String solution(int num) {
+        String answer = "";
+
+        if (num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+
+        return answer;
+    }
+}
