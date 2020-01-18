453e345c17d038b42fcc62ab9963ef795cfe1905
JHyeok <dev.jhyeok@gmail.com>
2020-01-18 20:09:57 +0900

    문자열을정수로바꾸기 추가

---
+++ src/main/level1/문자열을정수로바꾸기/OtherSolution.java
@@ -0,0 +1,23 @@
+package main.level1.문자열을정수로바꾸기;
+
+class OtherSolution {
+    public int getStrToInt(String str) {
+        boolean Sign = true;
+        int result = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '-')
+                Sign = false;
+            else if (ch != '+')
+                result = result * 10 + (ch - '0');
+        }
+        return Sign ? 1 : -1 * result;
+    }
+
+    //아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String args[]) {
+        OtherSolution strToInt = new OtherSolution();
+        System.out.println(strToInt.getStrToInt("-1234"));
+    }
+}
---
+++ src/main/level1/문자열을정수로바꾸기/Solution.java
@@ -0,0 +1,8 @@
+package main.level1.문자열을정수로바꾸기;
+
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
