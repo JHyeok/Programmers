7044438741bc14227687ec217a4b3472f5bce867
JHyeok <dev.jhyeok@gmail.com>
2020-05-23 17:50:39 +0900

    snippet2.py 수정

--- src/main/snippet2.py
+++ src/main/snippet2.py
@@ -9,6 +9,14 @@ for i in range(1, len(pool) + 1):
 overlap_num_list = list(set(num_list))
 print(overlap_num_list)
 
+# 순열과 조합 차이
+iterable_list = ['1', '2', '3', '4']
+print('순열 : ', list(map(''.join, itertools.permutations(iterable_list, 2))))
+print('조합 : ', list(map(''.join, itertools.combinations(iterable_list, 2))))
+
+# 순열 : ['12', '13', '14', '21', '23', '24', '31', '32', '34', '41', '42', '43']
+# 조합 : ['12', '13', '14', '23', '24', '34']
+
 # 배열에서 중복 제거
 ex_list = [1, 1, 2, 3, 3]
 overlap_ex_list = list(set(ex_list))
